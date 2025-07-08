279fab5727c096edd4b65ced2d6d1e03763c799c
desan <desanctisisabella95@gmail.com>
2025-07-08 22:57:57 +0200

    inizio implementaz app

--- src/main/java/com/develhope/interceptior_middelware/controller/BasicController.java
+++ src/main/java/com/develhope/interceptior_middelware/controller/BasicController.java
@@ -1,4 +1,10 @@
 package com.develhope.interceptior_middelware.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/time")
 public class BasicController {
+
 }
--- src/main/java/com/develhope/interceptior_middelware/controller/LegacyController.java
+++ src/main/java/com/develhope/interceptior_middelware/controller/LegacyController.java
@@ -1,4 +1,11 @@
 package com.develhope.interceptior_middelware.controller;
-
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/legacy")
 public class LegacyController {
+
+    @GetMapping("")
+    public String oldCode() {
+        return "this is an old code";
+    }
 }

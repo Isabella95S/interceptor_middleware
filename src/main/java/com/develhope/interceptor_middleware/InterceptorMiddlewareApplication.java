0beb1a75dc79b3d17f9665eec8d6fbf74abd1161
desan <desanctisisabella95@gmail.com>
2025-07-13 15:07:29 +0200

    fixed pom, application.properties, ecc..

---
+++ src/main/java/com/develhope/interceptor_middleware/InterceptorMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.develhope.interceptor_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/develhope/interceptor_middleware/configurations/SpringMVCConfiguration.java
@@ -0,0 +1,18 @@
+package com.develhope.interceptor_middleware.configurations;
+
+import com.develhope.interceptor_middleware.interceptors.APILoggingInterceptor;
+import com.develhope.interceptor_middleware.interceptors.LegacyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Component
+public class SpringMVCConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private LegacyInterceptor legacyInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(legacyInterceptor).addPathPatterns("/legacy/**","/legacy*");
+    }
+}
